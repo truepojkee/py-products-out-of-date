0c2a448a4e6e1e08a2218dbcbd25730239be1f04
Pojkee <cbo6oda22@gmail.com>
2026-06-11 23:03:11 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -4,27 +4,28 @@ from unittest import mock
 
 
 @mock.patch("app.main.datetime.date")
-def test_outdated_products(mock_date: int) -> None:
+def test_outdated_products(mock_date) -> None:
     real_date = datetime.date
     mock_date.today.return_value = real_date(2026, 6, 11)
-    mock_date.side_effect = lambda *args, **kwargs: (
-        real_date(*args, **kwargs))
+    mock_date.side_effect = lambda *args, **kwargs: real_date(*args, **kwargs)
+
     products = [
         {
             "name": "salmon",
-            "expiration_date": datetime.date(2025, 10, 2),
+            "expiration_date": real_date(2025, 10, 2),
             "price": 600
         },
         {
             "name": "chicken",
-            "expiration_date": datetime.date(2027, 10, 22),
+            "expiration_date": real_date(2022, 10, 22),
             "price": 120
         },
         {
             "name": "duck",
-            "expiration_date": datetime.date(2026, 6, 9),
+            "expiration_date": real_date(2026, 6, 9),
             "price": 160
         }
     ]
+
     result = outdated_products(products)
-    assert result == ["salmon", "duck"]
+    assert result == ["salmon", "chicken", "duck"]
