8037dbcdd18b44382dbe33b2b86e50006f21b917
Pojkee <cbo6oda22@gmail.com>
2026-06-11 23:16:04 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,24 +2,28 @@ from app.main import outdated_products
 import datetime
 from unittest import mock
 
-@mock.patch("app.main.datetime.date", wraps=datetime.date)
-def test_outdated_products(mock_date: mock.MagicMock) -> None:
-    mock_date.today.return_value = datetime.date(2026, 6, 11)
+real_date = datetime.date
+
+
+@mock.patch("app.main.datetime.date")
+def test_outdated_products(mock_date: int) -> None:
+    mock_date.today.return_value = real_date(2026, 6, 11)
+    mock_date.side_effect = lambda *args, **kwargs: real_date(*args, **kwargs)
 
     products = [
         {
             "name": "salmon",
-            "expiration_date": datetime.date(2025, 10, 2),
+            "expiration_date": real_date(2025, 10, 2),
             "price": 600
         },
         {
             "name": "chicken",
-            "expiration_date": datetime.date(2022, 10, 22),
+            "expiration_date": real_date(2022, 10, 22),
             "price": 120
         },
         {
             "name": "duck",
-            "expiration_date": datetime.date(2026, 6, 9),
+            "expiration_date": real_date(2026, 6, 9),
             "price": 160
         }
     ]
